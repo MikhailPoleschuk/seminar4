b61a8cd74da8f5972869d5d6fbbc88d00bcc15e6
mikle <mr.mikle@mail.ru>
2022-09-28 10:50:23 +0300

    tasks

---
+++ task27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+Console.Clear();
+Console.WriteLine("Введите N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int newN = N;
+while (newN >= 10) // обязательно больше или равно иначе при числе 10001 получим 11
+{
+    sum = sum + newN % 10;
+    newN = newN / 10;
+}
+sum=sum+newN;
+Console.WriteLine(sum);
---
+++ task29/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+//5 элементов  1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+//3 элемента  6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+Console.WriteLine("Введите N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] ZapolnimArr(int[] ar)
+{
+    for (int i = 0; i < ar.Length; i++)
+    {
+        ar[i] = new Random().Next(0, 100);
+    }
+    return ar;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($" {arr[i]} |");
+    }
+}
+
+
+int[] array = new int[N];
+array=ZapolnimArr(array);
+
+//{ x(), x(), x(), x(), x(), x(), x(), x()};
+PrintArray(array);
